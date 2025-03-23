7913f5b40ac8642c5c50e6cf77d414b480911cf2
doctore-source <jami__h@hotmail.com>
2025-03-23 09:31:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,4 @@ st.title("Doctore:
 st.title("Welcome to Doctore!")
 st.write("This is where your custom content, data, and visuals will go.")
 nohup streamlit run streamlit_app.py --server.port 8501 --server.address 0.0.0.0 > dashboard_log.out 2>&1 &
+python3 generate_reports.py
