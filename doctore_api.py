2eecf8719f442213036426ed14b561c3a2327865
doctore-source <jami__h@hotmail.com>
2025-03-23 13:52:22 +0200

    Update doctore_api.py

--- doctore_api.py
+++ doctore_api.py
@@ -10,3 +10,5 @@ if response.status_code == 200:
     st.write("Prediction:", result['prediction'])
 else:
     st.write("Error:", response.text)
+    curl -X POST http://127.0.0.1:5000/predict -H "Content-Type: application/json" -d '{"features": [1, 2, 3, 4, 5]}'
+    
