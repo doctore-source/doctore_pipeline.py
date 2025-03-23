3be50e232ff5fe2217a3460565e3b9390e143800
doctore-source <jami__h@hotmail.com>
2025-03-23 23:12:27 +0200

    Update python3 doctore_api.py

--- python3 doctore_api.py
+++ python3 doctore_api.py
@@ -1,1 +1,9 @@
 sudo systemctl status doctore_api.service
+@app.route('/health', methods=['GET'])
+def health():
+    status = {
+        "status": "API is running",
+        "model_loaded": model is not None
+    }
+    return jsonify(status), 200
+    
